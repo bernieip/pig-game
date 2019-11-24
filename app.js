7e61447b410a8c6ee58aad6625d8044acf26c640
bernieip <bernie.ip414@gmail.com>
2019-11-23 20:13:36 -0500

    finish pig game

--- app.js
+++ app.js
@@ -9,60 +9,56 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, dice, activePlayer, gamePlaying;
 
 init();
 
-
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    if(gamePlaying) {
-        // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+    if (gamePlaying) {
+        // 1. Roll the dice to generate random number
+        dice = Math.floor(Math.random() * 6) + 1;
 
-        //2. Display the result
+        // 2. display the results
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
 
-        //3. Update the round score IF the rolled number was NOT a 1
+        // 3. Update the round score IF the rolled number was NOT a 1
         if (dice !== 1) {
-            //Add score
+            // add score
             roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;    
         } else {
-            //Next player
+            // next player
             nextPlayer();
-        }
-    }    
+        }    
+    }
 });
-
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
-        // Add CURRENT score to GLOBAL score
+        // add current score to global score
         scores[activePlayer] += roundScore;
-
-        // Update the UI
+        
+        // update the UI with the updated score
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        // check if player won the game
+        if (scores[activePlayer] >= 20) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
-            //Next player
-            nextPlayer();
+        // next player
+        nextPlayer();   
         }
     }
 });
 
-
 function nextPlayer() {
-    //Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -72,11 +68,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    //document.querySelector('.player-0-panel').classList.remove('active');
-    //document.querySelector('.player-1-panel').classList.add('active');
-
     document.querySelector('.dice').style.display = 'none';
-}
+};
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
@@ -84,9 +77,9 @@ function init() {
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
-    gamePlaying = true;
-    
-    document.querySelector('.dice').style.display = 'none';
+    gamePlaying = true;    
+
+    document.querySelector('.dice').style.display = 'none'; 
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -99,24 +92,4 @@ function init() {
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
-}
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
-
-
-
-
-
-
-
-
-/*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-*/
+};
